c88f2420b21f0fd7c8e26751b4948b8f4ab15f4d
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-10-31 10:30:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/anumitultra/searchbot /searchbot
+  git clone https://github.com/anumitultra/searchbot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /searchbot
